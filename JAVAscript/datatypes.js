f50ecaad1151b7c7ee96a1fc6075bb6d471d0dd0
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2024-07-13 17:30:18 +0530

    kmm

---
+++ JAVAscript/datatypes.js
@@ -0,0 +1,5 @@
+var a =false
+console.log(typeof(a))
+
+var b = true
+console.log(typeof(b))
---
+++ JAVAscript/task3.js
@@ -0,0 +1,36 @@
+function add()
+{
+    a = 10
+    b =3403
+    console.log(a*b)
+}
+
+add()
+
+
+function fav()
+{
+var Actor = "kamal"
+var Player = " Dhoni"
+var movie = "vikram"
+
+console.log("favourite Actor"+ Actor)
+console.log("Favourite player"+Player)
+console.log("Favourite movie"+movie)
+
+}
+
+
+fav()
+
+
+//parameters
+
+function area(){
+    var len = 20
+    var brth = 30
+    area_parameter = len * brth
+    console.log("area of parameters:"+area_parameter)
+}
+
+area()
